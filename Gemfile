4b16057bd0133018efa03a4e06c7bdb24fa74f96
Stephen Hardisty <shardisty@etsy.com>
2014-11-09 17:23:18 -0500

    beginnings of actual site!

--- Gemfile
+++ Gemfile
@@ -1,5 +1,15 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+ruby '2.1.4'
+
 gem 'sinatra'
 gem 'sinatra-assetpack'
+gem 'haml'
+gem 'sinatra-contrib'
+gem 'sass'
+gem 'bourbon'
+
+group :development do
+  gem 'sinatra-reloader'
+end
--- drlaminator.rb
+++ drlaminator.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
 require 'sinatra/assetpack'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'haml'
+require 'sass'
 
 class DrLaminator < Sinatra::Base
   register Sinatra::AssetPack
+  helpers Sinatra::ContentFor
+  register Sinatra::Reloader if self.development?
+
+  assets do
+    serve '/css', from: 'css'
+
+    css :application, [
+        '/css/*.css'
+    ]
+
+    css_compression :sass
+  end
 
   get '/' do
-    'sup'
+    haml :index, format: :html5
   end
 end
